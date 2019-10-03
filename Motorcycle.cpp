7777941db2883f8f9218dd821a3aea6dbcd0e32e
btannuzzo <b.y.tannuzzo@gmail.com>
2019-10-03 16:45:50 -0400

    Added comments to function (especially mileage)

--- Motorcycle.cpp
+++ Motorcycle.cpp
@@ -5,20 +5,22 @@
 
 #include "Motorcycle.h"
 
+//Constructor
 Motorcycle::Motorcycle(string brand, string model, string fuelType){
 	setBrand(brand);
 	setModel(model);
 	setFuelType(fuelType);
 }
 
+//Destructor
 Motorcycle::~Motorcycle() = default;
 
 
-
+//Mileage function
 double Motorcycle::mileageEstimate(double time){
-	double mileage = 10 * time;
-	if(fuelType == "premium"){
-		mileage += mileage * 0.10;
+	double mileage = 10 * time;		//mileage = 10 miles per minute	
+	if(fuelType == "premium"){		//if fuel is premium
+		mileage += mileage * 0.10;	//boost mileage by 10%
 	}
 	
 	return mileage;
