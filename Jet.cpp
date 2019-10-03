b2bce92779d0ed1c7fc7de25f241de75a09ff542
btannuzzo <b.y.tannuzzo@gmail.com>
2019-10-02 21:10:26 -0400

    Added constructor and destructor

---
+++ Jet.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Betty Tannuzzo
+//
+
+#include "Jet.h"
+
+
+Jet::Jet(string brand, string model, string fuelType, int numberOfEngines){
+	setBrand(brand);
+	setModel(model);
+	setFuelType(fuelType);
+	setNumberOfEngines(numberOfEngines);
+}
+
+
+Jet::~Jet() = default;
