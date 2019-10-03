8022d481febdc6b07011baeca8cfd5234b4cf9a7
btannuzzo <b.y.tannuzzo@gmail.com>
2019-10-02 22:14:28 -0400

    Coded mileage function

--- Skateboard.cpp
+++ Skateboard.cpp
@@ -9,3 +9,25 @@ Skateboard::Skateboard(string brand, string model){
 	setModel(model);
 }
 
+
+Skateboard::~Skateboard() = default;
+
+
+double Skateboard::mileageEstimate(double time){
+	double mileage = rand() % 41 + 10;
+	mileage /= 100;
+	mileage *= time; 
+	
+	double thirdOfTime = time/3;
+	thirdOfTime++;
+	double randTime;	
+
+	if(time > 50 && time < 250){
+		randTime = rand() % thirdOfTime + 1;
+		mileage = randTime;	
+	}
+	
+ 	return mileage;
+}
+
+
