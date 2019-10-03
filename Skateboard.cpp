1e894bfdaee53fe1ca161993fcca8746f7e9468d
btannuzzo <b.y.tannuzzo@gmail.com>
2019-10-02 22:17:00 -0400

    Added comments to functions

--- Skateboard.cpp
+++ Skateboard.cpp
@@ -4,15 +4,16 @@
 
 #include "Skateboard.h"
 
+//Constructor
 Skateboard::Skateboard(string brand, string model){
 	setBrand(brand);
 	setModel(model);
 }
 
-
+//Destructor
 Skateboard::~Skateboard() = default;
 
-
+//Calculate mileage
 double Skateboard::mileageEstimate(double time){
 	double mileage = rand() % 41 + 10;
 	mileage /= 100;
