ac2b2a8b201ab38c8419827ed1e9fa5aef507545
btannuzzo <b.y.tannuzzo@gmail.com>
2019-10-03 16:41:06 -0400

    Added constructor and destructor to cpp file

---
+++ Motorcycle.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Betty Tannuzzo on 10/03/19
+//
+
+
+#include "Motorcycle.h"
+
+Motorcycle::Motorcycle(string brand, string model, string fuelType){
+	setBrand(brand);
+	setModel(model);
+	setFuelType(fuelType);
+}
+
+Motorcycle::~Motorcycle() = default;
+
+
