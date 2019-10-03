0415f3bc963039ff4310beb6f2ed942b27be8c49
btannuzzo <b.y.tannuzzo@gmail.com>
2019-10-03 16:42:58 -0400

    Added mileage function to cpp file

--- Motorcycle.cpp
+++ Motorcycle.cpp
@@ -14,3 +14,12 @@ Motorcycle::Motorcycle(string brand, string model, string fuelType){
 Motorcycle::~Motorcycle() = default;
 
 
+
+double Motorcycle::mileageEstimate(double time){
+	double mileage = 10 * time;
+	if(fuelType == "premium"){
+		mileage += mileage * 0.10;
+	}
+	
+	return mileage;
+}
